c1d4a3dd5b81f0e754e4ae848e713caf182ec71e
kevinmiza <kevinmiza@gmail.com>
2019-08-10 09:09:26 -0600

    Feature1

---
+++ Ejercicio5/src/ejercicio5/Calculadora.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5;
+
+/**
+ *
+ * @author KEVIN MIZA
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        
+    public static int sumarNumeros(int numero1, int numero2){
+    return numero1+numero2;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int num1 = 6;
+        int num2 =5;
+        
+        int resultado = sumarNumeros(num1,num2);
+        System.out.println(resultado);
+    }
+
+}
